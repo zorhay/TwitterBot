34db7cc5b37d3537f12c90863dc760ee54d5fc93
Zori <zor.hay@gmail.com>
2018-06-22 10:17:02 +0400

    Added tweet filter operations.

--- src/operations.py
+++ src/operations.py
@@ -1,13 +1,3 @@
-import tweepy
-import auth
-
-api = auth.authentication()
-
-
-def follow():
-    for follower in tweepy.Cursor(api.followers).items():
-        follower.follow()
-        print ("Followed everyone that is following " + user.name)
 
 def get_user_tweets(username, count):
 	return api.user_timeline(screen_name = username, count = count, include_rts = True)
@@ -31,12 +21,3 @@ def normalise_tweet(tweet_text):
     while '  ' in tweet_text:
         tweet_text = tweet_text.replace('  ', ' ')
     return tweet_text.strip()
-
-if __name__ == '__main__':
-    print (clean_tweet_from_usernames('dasj dasb dasjdhaj     @sjdbad dhajsdh'))
-    # tweets = get_user_tweets('__humanx__', 10)
-    # for tweet in tweets:
-    #     if tweet._json.get('in_reply_to_status_id') is None:
-    #         print(tweet._json.get('text'))
-
-
