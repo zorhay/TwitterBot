ba5979f39100bd94ee1bf541e2e853f1494d9b55
Zori Hayrapetyan <Zor.hay@gmail.com>
2019-11-29 11:09:27 +0400

    Added WordRNN

--- src/poebot/rnn_interface.py
+++ src/poebot/rnn_interface.py
@@ -1,6 +1,13 @@
 # RNN ֊ https://github.com/pytorch/examples/tree/master/word_language_model
 from .text_analysis import ArmenianLanguage
 import random
+from ..word_language_model.internal_generate import generate
+
+
+def _shuffle_text(text):
+    text_words = text.split()
+    random.shuffle(text_words)
+    return text_words
 
 
 class WordRandom(object):
@@ -57,7 +64,39 @@ class WordRandom(object):
 
 
 class WordRNN(object):
-    pass
+    def __init__(self):
+        pass
+
+    def get_line(self, syllable_count):
+        if syllable_count <= 0:
+            return ''
+        words = self._generate(prime=None, words=syllable_count).split(' ')
+
+        while True:
+            for word in words:
+                taken_syllables = len(self.language.divided_into_syllables(word))
+            if taken_syllables > syllable_count:
+                words.pop()
+            else:
+                return ' '.join(words)
+
+    def end_with(self, line_ending, syllable_count, exception_word=''):
+        pass
+
+    def _find_word_with_ending(self, ending, exception_word):
+        shuffled_words = _shuffle_text(self.text_words)
+        for word in self.text_words:
+            if word.endswith(ending) and word != exception_word:
+                return word
+        else:
+            return exception_word
+
+    def _shuffle_text(self):
+        pass
+
+    def _generate(self, prime, words):
+        return generate(data_src='data/', checkpoint='./model.pt', prime=prime, words=words,
+                        seed=1111, cuda=False, temperature=1.0, log_interval=100)
 
 
 class CharRNN(object):
